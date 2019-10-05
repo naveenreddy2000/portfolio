4cee159d216aa2404e7416a015ba4a85aa4b40ec
naveenreddy2000 <konugantinaveenreddy@gmail.com>
2019-10-05 21:31:34 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 8000;
+const path = require('path')
+
+app.use(express.static('public'));
+
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/' + 'public/templates/index.html');
+});
+
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!\n /`));
+
+
---
+++ public/js/javascript.js
@@ -0,0 +1,22 @@
+
+
+$("#img1").hover(function(){
+    $("#img1").fadeTo("slow",0.2);
+    this.style.cursor="pointer";
+  },function(){
+    $("#img1").fadeTo("slow",1);
+  });
+
+$("#img2").hover(function(){
+    $("#img2").fadeTo("slow",0.2);
+    this.style.cursor="pointer";
+  },function(){
+    $("#img2").fadeTo("slow",1);
+  });
+
+  $("#img3").hover(function(){
+    $("#img3").fadeTo("slow",0.2);
+    this.style.cursor="pointer";
+  },function(){
+    $("#img3").fadeTo("slow",1);
+  });
