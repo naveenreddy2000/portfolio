dd86f480011960cd23a89fcdcf3b46e4d12cc442
naveenreddy2000 <konugantinaveenreddy@gmail.com>
2019-10-06 10:34:25 +0530

    modified

--- index.js
+++ index.js
@@ -5,24 +5,6 @@ const port = process.env.PORT || 8000;
 app.use(express.static('public'));
 
 
-/*var compress_images = require('compress-images'), INPUT_path_to_your_images, OUTPUT_path;
- 
-INPUT_path_to_your_images = './public/src/img/gudavalli_branch.jpg';
-OUTPUT_path = './public/img/';
-
-compress_images(INPUT_path_to_your_images, OUTPUT_path, {compress_force: false, statistic: true, autoupdate: true}, false,
-                                            {jpg: {engine: 'mozjpeg', command: ['-quality', '60']}},
-                                            {png: {engine: 'pngquant', command: ['--quality=20-50']}},
-                                            {svg: {engine: 'svgo', command: '--multipass'}},
-                                            {gif: {engine: 'gifsicle', command: ['--colors', '64', '--use-col=web']}}, function(error, completed, statistic){
-            console.log('-------------');
-            console.log(error);
-            console.log(completed);
-            console.log(statistic);
-            console.log('-------------');                                   
-});*/
-
-
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/' + 'public/templates/index.html');
 });
--- public/js/javascript.js
+++ public/js/javascript.js
@@ -1,22 +1,22 @@
 
 
 $("#img1").hover(function(){
-    $("#img1").fadeTo("slow",0.2);
+    $("#img1").fadeTo(50,0.2);
     this.style.cursor="pointer";
   },function(){
-    $("#img1").fadeTo("slow",1);
+    $("#img1").fadeTo(50,1);
   });
 
 $("#img2").hover(function(){
-    $("#img2").fadeTo("slow",0.2);
+    $("#img2").fadeTo(50,0.2);
     this.style.cursor="pointer";
   },function(){
-    $("#img2").fadeTo("slow",1);
+    $("#img2").fadeTo(50,1);
   });
 
   $("#img3").hover(function(){
-    $("#img3").fadeTo("slow",0.2);
+    $("#img3").fadeTo(50,0.2);
     this.style.cursor="pointer";
   },function(){
-    $("#img3").fadeTo("slow",1);
+    $("#img3").fadeTo(50,1);
   });
