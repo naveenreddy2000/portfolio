ec1b053329bac82552b1a86d79093ea5e9286391
naveenreddy2000 <konugantinaveenreddy@gmail.com>
2019-10-07 11:40:27 +0530

    modified

--- public/js/javascript.js
+++ public/js/javascript.js
@@ -1,22 +1,35 @@
 
+$(".centered").hide();
 
 $("#img1").hover(function(){
-    $("#img1").fadeTo(50,0.2);
+    $("#img1").fadeTo(50,0.5);
     this.style.cursor="pointer";
+    $("#txt1").show();
+    $("#txt1").fadeTo(1,1);
   },function(){
     $("#img1").fadeTo(50,1);
+    $("#txt1").hide();
+    $("#txt1").fadeTo(1,0);
   });
 
 $("#img2").hover(function(){
-    $("#img2").fadeTo(50,0.2);
+    $("#img2").fadeTo(50,0.5);
     this.style.cursor="pointer";
+    $("#txt2").show();
+    $("#txt2").fadeTo(1,1);
   },function(){
     $("#img2").fadeTo(50,1);
+    $("#txt2").hide();
+    $("#txt2").fadeTo(1,0);
   });
 
   $("#img3").hover(function(){
-    $("#img3").fadeTo(50,0.2);
+    $("#img3").fadeTo(50,0.5);
     this.style.cursor="pointer";
+    $("#txt3").show();
+    $("#txt3").fadeTo(1,1);
   },function(){
     $("#img3").fadeTo(50,1);
+    $("#txt3").hide();
+    $("#txt3").fadeTo(1,0);
   });
